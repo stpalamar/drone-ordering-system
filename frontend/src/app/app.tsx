ac24ca80e667201e476065c233f5748eff1f6868
stpalamar <stanislav6121@gmail.com>
2024-04-04 02:06:14 +0300

    feat: initial commit

---
+++ frontend/src/app/app.tsx
@@ -0,0 +1,5 @@
+const App: React.FC = () => {
+    return <>Hello World!</>;
+};
+
+export { App };
---
+++ frontend/src/main.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App } from './app/app.js';
+
+createRoot(document.querySelector('root') as HTMLElement).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
